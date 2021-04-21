60694f9273b91aab4c55e891c156d8def1ecc418
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-21 22:52:40 +0200

    Add router-view

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import Vuex from 'vuex';
+import router from './router';
 import { store } from './store/store';
 
 Vue.config.productionTip = false;
@@ -9,5 +10,6 @@ Vue.use(Vuex);
 
 new Vue({
   store: store,
+  router,
   render: (h) => h(App),
 }).$mount('#app');
