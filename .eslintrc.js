e167af5a8cbf27e65944ad3605ec521e24fe8fe1
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-21 21:44:14 +0200

    Add ESLint and set rules

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "node": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "prettier"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "prettier"
+    ],
+    "rules": {
+      "prettier/prettier": "error",
+    }
+};
