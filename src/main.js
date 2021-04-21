d66102c622e79cb51438f931220a48d0055b23f5
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-21 20:06:36 +0200

    Fix naming store

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Vuex from 'vuex'
-import store from './store'
+import { store } from './store/store'
 
 Vue.config.productionTip = false
 
 Vue.use(Vuex)
 
 new Vue({
-  store,
+  store: store,
   render: h => h(App),
 }).$mount('#app')
