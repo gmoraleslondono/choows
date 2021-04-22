42e80e5fcf402cb9f8d08053d95aa496655ddcd7
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-22 20:22:13 +0200

    Add footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="footer">
+    <p>By Gloria Morales 2021</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+.footer {
+  width: 100%;
+  margin: 0 auto;
+  background-color: #121212;
+  color: white;
+  font-size: 18px;
+  text-align: center;
+  padding: 1%;
+  position: sticky;
+  bottom: 0;
+}
+</style>
