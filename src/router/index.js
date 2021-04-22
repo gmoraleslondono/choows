52abfec095d4adb6640ed3b0b1945dbcbf858f16
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-23 01:06:22 +0200

    Create favorites page

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
 import TvShowDetails from '../views/TvShowDetails.vue';
+import Favorites from '../views/Favorites.vue';
 
 Vue.use(VueRouter);
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/tvShowDetails',
     name: 'tvShowDetails',
     component: TvShowDetails,
+  },
+  {
+    path: '/favorites',
+    name: 'favorites',
+    component: Favorites,
   },
 ];
 
---
+++ src/views/Favorites.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="favorites">
+    <Header />
+    <div class="content">
+      <h1>My Favorites Shows</h1>
+      <TvShowCard :btnType="'remove'" />
+    </div>
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue';
+import Header from '../components/Header.vue';
+import TvShowCard from '../components/TvShowCard.vue';
+
+export default {
+  name: 'Favorites',
+  components: {
+    Header,
+    Footer,
+    TvShowCard,
+  },
+  data() {
+    return {};
+  },
+  methods: {
+    removeFavorite() {
+      console.log('remove from favorites');
+    },
+  },
+};
+</script>
+
+<style scoped>
+.favorites {
+  width: 100%;
+  min-height: 100vh;
+  margin: 0 auto;
+  background-color: #00b2ff;
+}
+
+.content {
+  width: 80%;
+  margin: 0 auto;
+}
+
+h1 {
+  text-align: center;
+  margin: 5% 0;
+}
+</style>
