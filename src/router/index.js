0c1687d3702112928da3100fcd3e8c683a466b22
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-22 23:58:28 +0200

    Create TvShowDetails view

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import TvShowDetails from '../views/TvShowDetails.vue';
 
 Vue.use(VueRouter);
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home,
+  },
+  {
+    path: '/tvShowDetails',
+    name: 'tvShowDetails',
+    component: TvShowDetails,
   },
 ];
 
