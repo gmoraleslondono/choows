faf201e2bf7bb58aa7e5d39dbdaa8d5fe133493b
gmoraleslondono <gmoraleslondono@gmail.com>
2021-04-25 19:30:30 +0200

    Search TV shows by name

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,57 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-  // data in the component
-  state: {},
+  state: {
+    shows: [],
+    showSearchList: [],
+  },
 
-  getters: {},
+  getters: {
+    items: (state) => {
+      return state.shows;
+    },
+    resultList: (state) => {
+      return state.showSearchList;
+    },
+  },
 
-  // methods that changes the state
-  mutations: {},
+  actions: {
+    getShows({ commit }) {
+      axios.get('https://api.tvmaze.com/shows?page=0').then((response) => {
+        console.log('response', response);
+        commit('setShows', response.data);
+      });
+    },
+    async getShowsSearchList({ commit, state }, searchText) {
+      try {
+        await axios
+          .get('http://api.tvmaze.com/search/shows?', {
+            params: {
+              q: searchText,
+            },
+          })
+          .then((response) => {
+            console.log('result  by search', response.data);
+            state.showSearchList = response.data;
+            commit('setResultSearch', response.data);
+          });
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 
-  // methods in the component
-  actions: {},
-  modules: {},
+  mutations: {
+    setShows(state, shows) {
+      state.shows = shows;
+    },
+    setResultSearch(state, data) {
+      state.error = false;
+      state.showSearchList = data;
+    },
+  },
 });
