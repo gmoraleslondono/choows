05171c5b0f2f6c728052e125cdd31e725fb752b1
gmoraleslondono <gmoraleslondono@gmail.com>
2021-05-03 01:54:03 +0200

    Fix header mobile view

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -58,41 +58,41 @@ export default {
 }
 
 @media screen and (max-width: 1024px) {
+  .header-content {
+    width: 80%;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding-top: 10%;
+    gap: 5%;
+  }
+
   .menu {
     display: flex;
-    flex-direction: row;
-    justify-content: flex-end;
-    gap: 30px;
-    width: 100%;
-  }
-
-  .btn-header {
-    margin: 0;
-  }
-}
-
-@media screen and (max-width: 767px) {
-  .header-content {
     flex-direction: column;
-    align-items: center;
+    justify-content: center;
   }
 
   .btn-header {
-    font-size: 1.2rem;
     margin: 0;
-  }
-
-  .menu {
-    margin-top: 10%;
-    justify-content: space-between;
-    gap: 0px;
   }
 
   .border {
     border: 1px solid white;
     border-radius: 10px;
     padding: 5px 10px;
-    width: 40%;
+    width: 100%;
+    margin-bottom: 10px;
+  }
+}
+
+@media screen and (max-width: 812px) {
+  .header-content {
+    width: 90%;
+  }
+
+  .btn-header {
+    font-size: 1.2rem;
   }
 }
 </style>
